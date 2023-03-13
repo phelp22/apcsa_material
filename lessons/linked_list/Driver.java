85ee853109b560f161643c42a6379a35f74cd028
phelp22 <124067402+phelp22@users.noreply.github.com>
2023-03-12 22:35:43 -0400

    Update Driver.java

--- lessons/linked_list/Driver.java
+++ lessons/linked_list/Driver.java
@@ -5,7 +5,11 @@ import java.util.*;
 public class Driver{
   public static void main(String[] args) {
     MyLinkedList myList = new MyLinkedList();
-    
+    myList.add("a");
+    myList.add("b");
+    myList.add("c");
+    myList.add("d");
+    myList.add("e");
     System.out.println(myList);
   }
 }
