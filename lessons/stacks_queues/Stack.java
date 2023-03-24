0496304270323e8aa250c536da3622f897c9fd9c
phelp22 <124067402+phelp22@users.noreply.github.com>
2023-03-24 10:15:10 -0400

    Update Stack.java

--- lessons/stacks_queues/Stack.java
+++ lessons/stacks_queues/Stack.java
@@ -1,17 +1,19 @@
 
 public class Stack{
   // This Stack class is designed for integer values >= 0
-
+  
   // Instance variables here
-
+  private Node tail;
  // COnstructor
   public Stack(){
-
+    tail.setNext(null);
   }   
 
   // Add element to stack
   public void push(int value){
-
+    node NewNode = new Node();
+    
+    
   }
 
   // Remove element from stack  
