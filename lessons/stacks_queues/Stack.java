37cc22049b81b65d0905d5ae4e312cae97543fb0
phelp22 <124067402+phelp22@users.noreply.github.com>
2023-03-27 19:32:05 -0400

    Update Stack.java

--- lessons/stacks_queues/Stack.java
+++ lessons/stacks_queues/Stack.java
@@ -17,7 +17,7 @@ public class Stack{
       head.setData(value);
       tail.setData(value);
     }
-    if (size() == 1) {
+    else if (size() == 1) {
       node NewNode = new Node(value);
       head.setNext(NewNode);
       tail = NewNode;
@@ -33,31 +33,46 @@ public class Stack{
   public int pop(){
     if (isEmpty == false) {
       Node walker = head;
-      if (
-      
+      while (head.getNext() != null) {
+        walker = walker.getNext();
+      }
+      tail = walker; 
+      return walker.getNext(); 
     }
+    else return -1;
   }
 
   // Get top element
   public int top(){
-
+    return tail.getData();
   }
 
   // Check is stack empty
   public boolean isEmpty(){
-
+    if (size() == 0) return true;
+    else return false;
   }
 
   // Return size stack
   public int size(){
-  
+    int counter = 0;
+    Node walker = head;
+      while (head.getNext() != null) {
+        walker = walker.getNext();
+        counter++;
+      }
+    return counter;
   }
 
   // Print stack
   public String toString(){
-
+    String accumulator = "";
+    Node walker = head;
+    for (int i = 0; i < size(); i++) {
+      accumulator += walker.getData();
+      walker = walker.getNext();
+    }
+    return accumulator;
   }
-
-
 }
 
