a40d9a182b22d2ca3876187f6dba925c08f801b9
phelp22 <124067402+phelp22@users.noreply.github.com>
2023-03-27 20:25:36 -0400

    Update Driver.java

--- lessons/stacks_queues/Driver.java
+++ lessons/stacks_queues/Driver.java
@@ -2,9 +2,7 @@ public class Driver {
   public static void main (String[] args) {
     Stack NewStack = new Stack();
     NewStack.push(1);
-    System.out.println(NewStack.size());
     NewStack.push(2);
-    System.out.println(NewStack);
     NewStack.push(3);
     NewStack.push(4);
     NewStack.push(5);
